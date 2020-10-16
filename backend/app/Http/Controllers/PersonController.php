462840d0648120685b019c4aa83784602ae38432
sergiotaerri <sergio2000jr@gmail.com>
2020-10-16 00:36:51 -0300

    make controllers; implementação de rotas;
    Implementação de rotas e  criação de controles,
    Implementação de lógica nos controllers de Pessoa, necessita testes
    com seeding de banco antes de passar funcionalidades a outras rotas

---
+++ backend/app/Http/Controllers/PersonController.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Response;
+use Illuminate\Http\Request;
+use App\Http\Resources\Person as PersonResource;
+use App\Models\Person;
+
+class PersonController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return PersonResource::collection(Person::all());
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'first_name' => 'required|max:60',
+            'second_name' => 'requiredmax:255',
+        ]);
+
+        $person = new Person($request->all());
+
+        $person->save();
+        return response()->json(['pessoa' => $person], Response::HTTP_OK);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        return new PersonResource(Person::findOrFail($id));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $person = Person::findOrFail($id);
+        $person->update($request->all());
+
+        return response()->json(['pessoa' => $person], Response::HTTP_OK);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        Person::destroy($id);
+    }
+
+}
--- backend/routes/api.php
+++ backend/routes/api.php
@@ -3,6 +3,10 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\PersonContactController;
+use App\Http\Controllers\ContactTypeController;
+use App\Http\Controllers\PersonController;
+
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -17,3 +21,21 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('pessoas-contato', [PersonContactController::class,  'index']);
+Route::post('pessoas-contato', [PersonContactController::class,  'store']);
+Route::get('pessoas-contato/{id}', [PersonContactController::class, 'show']);
+Route::put('pessoas-contato/{id}', [PersonContactController::class, 'update']);
+Route::delete('pessoas-contato/{id}', [PersonContactController::class, 'delete']);
+
+Route::get('pessoas', [PersonController::class,  'index']);
+Route::post('pessoas', [PersonController::class,  'store']);
+Route::get('pessoas/{id}', [PersonController::class, 'show']);
+Route::put('pessoas/{id}', [PersonController::class, 'update']);
+Route::delete('pessoas/{id}', [PersonController::class, 'delete']);
+
+Route::get('tipos-contato', [ContactTypeController::class,  'index']);
+Route::post('tipos-contato', [ContactTypeController::class,  'store']);
+Route::get('tipos-contato/{id}', [ContactTypeController::class, 'show']);
+Route::put('tipos-contato/{id}', [ContactTypeController::class, 'update']);
+Route::delete('tipos-contato/{id}', [ContactTypeController::class, 'delete']);
